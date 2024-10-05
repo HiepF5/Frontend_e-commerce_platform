89dbc51ac1e7dcbc596b72e4cbc895b9394ba7bd
Nguyen Cong Hiep <hiepf5.nguyencong.ptit@gmail.com>
2024-10-06 04:01:40 +0700

    landingpage

---
+++ src/features/LandingPage/pages/LandingPage.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css'
+import Hero from '../components/Hero/Hero';
+import Navbar from '../components/Navbar/Navbar';
+import Products from '../components/Products/Products';
+import TopProducts from '../components/TopProducts/TopProducts';
+import Banner from '../components/Banner/Banner';
+import Subscribe from '../components/Subscribe/Subscribe';
+import Testimonials from '../components/Testimonials/Testimonials';
+import Footer from '../components/Footer/Footer';
+import Popup from '../components/Popup/Popup';
+const LandingPage = () => {
+  const [orderPopup, setOrderPopup] = useState(false)
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup)
+  }
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: 'ease-in-sine',
+      delay: 100
+    })
+    AOS.refresh()
+  }, [])
+  return (
+    <div>
+      <Navbar handleOrderPopup={handleOrderPopup} />
+      <Hero handleOrderPopup={handleOrderPopup} />
+      <Products />
+      <TopProducts handleOrderPopup={handleOrderPopup} />
+      <Banner />
+      <Subscribe />
+      <Testimonials/>
+      <Footer />
+      <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
+    </div>
+  )
+}
+
+export default LandingPage
